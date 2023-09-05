2eefaa1268c429273a38fae9f8084fc46d88b7ba
Elimali hamza <elimal.hamza@gmail.com>
2023-09-05 10:01:38 -0700

    .

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i < 101 and i > 1:
-            print(", ", end="")
+            print(" ", end="")
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end="")
         elif i % 3 == 0:
