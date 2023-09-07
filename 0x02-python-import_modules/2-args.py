c89b6dfa4a76614082bdb70414f13a91b87bb84e
Elimali hamza <elimal.hamza@gmail.com>
2023-09-07 04:52:51 -0700

    .

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,14 @@
 if __name__ == "__main__":
     import sys
 
-    leng = len(sys.argv) -1
-    
+    leng = len(sys.argv) - 1
+
     if leng == 0:
         print("{} arguments.".format(leng))
-        
+
     elif leng == 1:
         print("{} argument:".format(leng))
-   
+
     else:
         print("{} arguments:".format(leng))
 
