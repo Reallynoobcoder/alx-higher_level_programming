c1641f9fde02ac36f9911cc4530810bf19877e7b
Elimali hamza <elimal.hamza@gmail.com>
2023-09-07 04:36:48 -0700

    .

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-leng = len(sys.argv)
+    leng = len(sys.argv)
 
-if leng == 1:
-    print("{} argument{}.".format(leng - 1, '' if leng == 2 else 's'))
-else:
-    print("{} arguments:".format(leng - 1))
+    if leng == 1:
+        print("{} argument{}.".format(leng - 1, '' if leng == 2 else 's'))
+    else:
+        print("{} arguments:".format(leng - 1))
 
-for i in range(1, leng):
-    print("{}: {}".format(i, sys.argv[i]))
+    for i in range(1, leng):
+        print("{}: {}".format(i, sys.argv[i]))
