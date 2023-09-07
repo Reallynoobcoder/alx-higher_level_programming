d16bbcbf444406ed5c34da64046844a3466ad40f
Elimali hamza <elimal.hamza@gmail.com>
2023-09-07 03:51:29 -0700

    task 1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+if __name__ = "__name__":
+    import calculator_1
+
+a = 10
+b = 5
+
+print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
+print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
+print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
+print("{} / {} = {}".format(a, b, calculator_1.div(a, b)))
