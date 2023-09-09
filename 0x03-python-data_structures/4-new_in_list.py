564a80e1d87276da1df3575016eca4b20619ce67
Elimali hamza <elimal.hamza@gmail.com>
2023-09-09 03:33:38 -0700

    .

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     cp_list = my_list.copy()
 
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return cp_list
 
     cp_list[idx] = element
