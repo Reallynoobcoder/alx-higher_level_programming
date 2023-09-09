9188ddc5bc91cc3306fe1d2253bab8d774b1e0cd
Elimali hamza <elimal.hamza@gmail.com>
2023-09-09 07:53:03 -0700

    .

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,67 +1,70 @@
-#include"lists.h"
 #include <stdlib.h>
-#include <stddef.h>
-listint_t *reverse_listint(listint_t **head)
+#include "lists.h"
+
+int is_palindrome(listint_t **head);
+
+listint_t *reverse_list(listint_t **head)
 {
-	listint_t *prev = NULL;
-	listint_t *next = NULL;
+	listint_t *node = *head, *next, *prev = NULL;
 
-	while (*head)
+	while (node)
 	{
-		next = (*head)->next;
-		(*head)->next = prev;
-		prev = *head;
-		*head = next;
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
 	}
-
 	*head = prev;
-
 	return (*head);
 }
 int is_palindrome(listint_t **head)
 {
-	if (*head == NULL || (*head)->next == NULL)
-		return 1;
+    listint_t *copy;
+    listint_t *curr;
+    listint_t *reversed;
+    listint_t *list1;
+    listint_t *list2;
+    listint_t *current;
 
-	listint_t *copy = NULL;
-	listint_t *curr = *head;
-	while (curr != NULL)
-	{
-		add_nodeint_end(&copy, curr->n);
-		curr = curr->next;
-	}
-	listint_t *reversed = reverse_listint(&copy);
+    if (*head == NULL || (*head)->next == NULL)
+        return 1;
+        
+    copy = NULL;
+    curr = *head;
+    
+    while (curr != NULL)
+    {
+        add_nodeint_end(&copy, curr->n);
+        curr = curr->next;
+    }
+    reversed = reverse_list(&copy);
 
-	listint_t *list1 = *head;
-	listint_t *list2 = reversed;
+    list1 = *head;
+    list2 = reversed;
 
-	while (list1 != NULL && list2 != NULL)
-	{
-		if (list1->n != list2->n)
-		{
-			listint_t *current;
+    while (list1 != NULL && list2 != NULL)
+    {
+        if (list1->n != list2->n)
+        {
+            listint_t *current;
 
-			while (copy != NULL)
-			{
-				current = copy;
-				copy = copy->next;
-				free(current);
-			}
-
-
-			return 0;
-		}
-		list1 = list1->next;
-		list2 = list2->next;
-	}
-
-	listint_t *current;
-
-	while (copy != NULL)
-	{
-		current = copy;
-		copy = copy->next;
-		free(current);
-	}
-	return (list1 == NULL && list2 == NULL);
+            while (copy != NULL)
+            {
+                current = copy;
+                copy = copy->next;
+                free(current);
+            }
+            return 0;
+        }
+        list1 = list1->next;
+        list2 = list2->next;
+    }
+    
+    while (copy != NULL)
+    {
+        current = copy;
+        copy = copy->next;
+        free(current);
+    }
+    return (list1 == NULL && list2 == NULL);
 }
