65561eaeee014febf4b6794d7ccc2e672c77cfab
Elimali hamza <elimal.hamza@gmail.com>
2023-09-26 06:24:36 -0700

    .

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,10 +6,11 @@ class Square:
     """Define method size of square"""
     def __init__(self, size=0):
         """initialization of a new square
+
         size is the size of new aquare
         """
         if not isinstance(size, int):
-            raise TypeError("size must be an intege")
+            raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
 
