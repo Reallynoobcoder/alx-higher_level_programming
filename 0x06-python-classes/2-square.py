285ba8f6470589e7ae2754a6ef24783d406160e5
Elimali hamza <elimal.hamza@gmail.com>
2023-09-26 05:23:06 -0700

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Define class Square"""
+
+
+class Square:
+    """Define method size of square"""
+    def __init__(self, size=0):
+        """initialization of a new square
+        size is the size of new aquare
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an intege")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
