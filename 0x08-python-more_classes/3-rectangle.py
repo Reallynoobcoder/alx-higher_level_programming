434c1b347910871cca5c53105b8333bd4e890f83
Elimali hamza <elimal.hamza@gmail.com>
2023-10-02 08:27:08 -0700

    .

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -47,12 +47,7 @@ class Rectangle:
         return '\n'.join(result)
 
     def __str__(self):
-        if self.__width == 0 or self.__height == 0:
-            return("")
-        lis = []
-        for i in range(self.__height):
-            for j in range(self.__width):
-                lis.append("#")
-            if i != self.__height - 1:
-                lis.append("\n")
-        return "".join(lis)
+        if self.__width == 0 or self.height == 0:
+            return ""
+        result = ["#" * self.__width] * self.__height
+        return '\n'.join(result)
