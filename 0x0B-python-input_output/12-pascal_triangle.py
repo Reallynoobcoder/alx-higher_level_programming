b1a769f0b18a7885d6bd171792bfe2f4dd4c3137
Elimali hamza <elimal.hamza@gmail.com>
2023-10-10 12:42:53 -0700

    task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Module that returns a list of lists of integers representing Pascal's"""
+
+
+def pascal_triangle(n):
+    """returns a list of lists of integers representing the Pascal's"""
+    if n <= 0:
+        return []
+
+    triangle = []
+    row = [1]
+
+    for _ in range(n):
+        triangle.append(row)
+        row = [1] + [row[i] + row[i + 1] for i in range(len(row) - 1)] + [1]
+
+    return triangle
