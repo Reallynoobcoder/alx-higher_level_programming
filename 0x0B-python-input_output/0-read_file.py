f60aab90a460db37a9e61ae514a75cb8024ffdce
Elimali hamza <elimal.hamza@gmail.com>
2023-10-10 03:17:33 -0700

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Define function that read files"""
+
+
+def read_file(filename=""):
+    """Reading file using 'with'"""
+    with open(filename, 'r') as file:
+        print(file.read())
