b1370333940020a7492a43ce3de3cde7f89be635
Elimali hamza <elimal.hamza@gmail.com>
2023-10-10 07:42:05 -0700

    task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Accept argument and add them into a python list, saving them to a file"""
+import sys
+
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file  \
+        = __import__('6-load_from_json_file').load_from_json_file
+    try:
+        johnson = load_from_json_file("add_item.json")
+
+    except FileNotFoundError:
+        johnson = []
+
+    for i in sys.argv[1:]:
+        johnson.append(i)
+
+    save_to_json_file(johnson, 'add_item.json')
