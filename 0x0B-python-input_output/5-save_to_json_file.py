15d20ac129a7a376fb05490629ed3422cd63c248
Elimali hamza <elimal.hamza@gmail.com>
2023-10-10 05:18:00 -0700

    .

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,6 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Writing to a JSON file using 'with'"""
-    j_str = json.dumps(my_obj)
     with open(filename, 'w') as file:
-        file.write(j_str)
+        file.write(json.dumps(my_obj))
