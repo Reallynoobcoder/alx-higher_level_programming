5340a46afdcc48ff02703696b668a054b3b2165f
Elimali hamza <elimal.hamza@gmail.com>
2023-10-10 07:47:41 -0700

    .

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,17 +3,15 @@
 import sys
 
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file  \
-        = __import__('6-load_from_json_file').load_from_json_file
-    try:
-        johnson = load_from_json_file("add_item.json")
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-    except FileNotFoundError:
-        johnson = []
+try:
+    johnson = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    johnson = []
 
-    for i in sys.argv[1:]:
-        johnson.append(i)
+for i in sys.argv[1:]:
+    johnson.append(i)
 
-    save_to_json_file(johnson, 'add_item.json')
+save_to_json_file(johnson, 'add_item.json')
