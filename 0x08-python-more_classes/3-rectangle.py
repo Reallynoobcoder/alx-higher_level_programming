190e3e023614a1ea0d8daf6456592587b129960b
Reallynoobcoder <133228964+Reallynoobcoder@users.noreply.github.com>
2023-10-27 12:43:31 +0100

    Update 3-rectangle.py
    .

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -45,14 +45,3 @@ class Rectangle:
             return ""
         result = ["#" * self.__width] * self.__height
         return '\n'.join(result)
-
-    def __str__(self):
-        if self.__width == 0 or self.__height == 0:
-            return ""
-        lis = []
-        for i in range(self.__height):
-            for j in range(self.__width):
-                lis.append("#")
-            if i != self.__height - 1:
-                lis.append("\n")
-        return "".join(lis)
