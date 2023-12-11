76d011c05ef76561cd724a8089707283ac0daa37
Elimali hamza <elimal.hamza@gmail.com>
2023-12-11 05:25:45 -0800

    .

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 
 const args = process.argv;
 
-if (args.lenght === 2) {
+if (args.length === 2) {
   console.log('No argument');
-} else if (args.lenght === 3) {
+} else if (args.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Argument found');
