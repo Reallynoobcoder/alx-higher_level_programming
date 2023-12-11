f82f8bbe166ed0e0ff91e84fe9f45aec0dd63645
Elimali hamza <elimal.hamza@gmail.com>
2023-12-11 09:55:47 -0800

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,20 +1,18 @@
 #!/usr/bin/node
 
-let max = parseInt(process.argv[2]);
-let second = parseInt(process.argv[3]);
+let max = Number.MIN_SAFE_INTEGER;
+let second = Number.MIN_SAFE_INTEGER;
 
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  for (let i = 4; i < process.argv.length; i++) {
-    const current = parseInt(process.argv[i]);
-    if (!isNaN(current)) {
-      if (current > max) {
-        second = max;
-        max = current;
-      } else if (current > second) {
-        second = current;
-      }
+  for (let i = 2; i < process.argv.length; i++) {
+    const curr = parseInt(process.argv[i]);
+    if (curr > max) {
+      second = max;
+      max = curr;
+    } else if (curr > second && curr < max) {
+      second = curr;
     }
   }
   console.log(second);
