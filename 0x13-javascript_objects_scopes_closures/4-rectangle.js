f2152ce11ee4383f94e00ab90bfbad0c9705ab14
Elimali hamza <elimal.hamza@gmail.com>
2023-12-12 09:00:31 -0800

    task '

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+
+  rotate () {
+    const swap = this.height;
+    this.height = this.width;
+    this.width = swap;
+  }
+
+  double () {
+    this.width = this.width + this.width;
+    this.height = this.height + this.height;
+  }
+}
+
+module.exports = Rectangle;
