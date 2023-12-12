f70f8c35022312ca85fb34ab0bbe25b3a5b1712b
Elimali hamza <elimal.hamza@gmail.com>
2023-12-12 09:59:01 -0800

    .

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,7 +2,8 @@
 
 exports.nbOccurences = function (list, searchElement) {
   let c = 0;
-  for (let i = 0; i < list.legth; i++) {
+
+  for (let i = 0; i < list.length; i++) {
     if (list[i] === searchElement) {
       c++;
     }
