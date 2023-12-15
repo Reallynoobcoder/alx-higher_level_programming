16dbdd8d697d62b52d34ce76b18108a909aaae4d
Elimali hamza <elimal.hamza@gmail.com>
2023-12-15 09:55:36 -0800

    task 6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""import SQLalchemy"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Class state that inherit from Base"""
+
+    __tablename__ = 'states'
+
+    id = Column(Integer,
+                autoincrement=True,
+                nullable=False,
+                primary_key=True)
+
+    name = Column(String(128), nullable=False)
