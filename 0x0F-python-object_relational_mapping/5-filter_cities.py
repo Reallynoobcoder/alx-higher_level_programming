e59cbc99927ff2fd638028502759b7f3c62c456c
Elimali hamza <elimal.hamza@gmail.com>
2023-12-15 06:26:32 -0800

    task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     JOIN states ON cities.state_id = states.id\
     WHERE states.name = %s\
     ORDER BY cities.id;"
-    
+
     cursor.execute(query, (state_agr,))
 
     rows = cursor.fetchall()
