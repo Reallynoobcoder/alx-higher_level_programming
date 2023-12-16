4a5a1f27e81cff3d6563c712b198f958ab774185
Elimali hamza <elimal.hamza@gmail.com>
2023-12-16 03:31:47 -0800

    task 9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""list all State objects that contain the letter a"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(argv[1], argv[2], argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states_with_a = session.query(State).filter
+    (State.name.like('%a%')).order_by(State.id).all()
+
+    print("{}: {}".format(states_with_a.id, states_with_a.name))
