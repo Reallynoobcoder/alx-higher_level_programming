744d671e3e2ef09ed0ef836d0a15aa47b985f8d8
Elimali hamza <elimal.hamza@gmail.com>
2023-12-16 03:12:12 -0800

    .

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
+"""prints the first State object from the database hbtn_0e_6_usa"""
 from sys import argv
-from model_state import State, Base
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     first_state = session.query(State).order_by(State.id).first()
 
     if first_state:
-        print("1:{}, Name: {}"
+        print("{}, {}"
               .format(first_state.id, first_state.name))
     else:
         print("Nothing")
