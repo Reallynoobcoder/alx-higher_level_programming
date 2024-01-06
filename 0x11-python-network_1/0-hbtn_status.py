aef60c7f5ba551c23fb8620bc685519843c86672
hamza elimali <elimal.hamza@gmail.com>
2024-01-06 08:50:44 -0800

    .

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,3 +5,7 @@ url = "https://alx-intranet.hbtn.io/status"
 
 with urllib.request.urlopen(url) as response:
     content = response.read().decode('utf-8')
+
+print("Body response:\n\t- type: {}\n\
+\t- content: {}\n\
+\t- utf8 content: {}".format(type(content), content, content))
