823778ce1c036109314bc7f7319faf87f539ad7b
hamza elimali <elimal.hamza@gmail.com>
2024-01-07 10:15:59 -0800

    task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+Sends a POST request to URL with a search letter as a parameter.
+If the response contains valid JSON, displays the id and name,
+otherwise handles errors.
+'''
+import sys
+import requests
+
+if __name__ == '__main__':
+    av1 = "" if len(sys.argv) == 1 else sys.argv[1]
+
+    data = {'q': av1}
+    r = requests.post('http://0.0.0.0:5000/search_user', data)
+
+    try:
+        json_data = r.json()
+        if not json_data:
+            print("No result")
+        else:
+            id = json_data.get('id')
+            name = json_data.get('name')
+            print(f"[{id}] {name}")
+    except ValueError:
+        print("Not a valid JSON")
