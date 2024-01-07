ea11b04c39035f57306a0b99cd87fc7f17486a26
hamza elimali <elimal.hamza@gmail.com>
2024-01-07 09:50:41 -0800

    task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Sends a POST request to a specified URL with an email parameter,
+ and displays the decoded content of the response'''
+
+from sys import argv
+from urllib import request, parse
+
+if __name__ == "__main__":
+    url = argv[1]
+    parameters = {'email': argv[2]}
+    
+    querystring = parse.urlencode(parameters)
+    data = querystring.encode('utf-8')
+    
+    with request.urlopen(url, data) as response:
+        content = response.read().decode('utf-8')
+    
+    print(content)
