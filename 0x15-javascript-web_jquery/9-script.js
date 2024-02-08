0830b2327fac6cd6d6ec5b80a1c8c3648b46bb85
elimali hamza <elimal.hamza@gmail.com>
2024-02-08 03:55:09 -0800

    Jquery

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,10 @@
+/* global $ */
+$(function () {
+  $.ajax({
+    type: 'GET',
+    url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
+    success: function (data) {
+      $('#hello').text(data.hello);
+    }
+  });
+});
